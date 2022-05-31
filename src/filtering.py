45c19bb6dc2905d4dbf945905bc6a7e9217579f0
hjgp <101342323+hjgp@users.noreply.github.com>
2022-06-01 00:30:33 +0900

    Add files via upload

---
+++ src/filtering.py
@@ -0,0 +1,19 @@
+import string
+import argparse
+from curse_detector import CurseDetector
+
+def filtering(opt):
+    curse = CurseDetector()
+    result = curse.predict(opt.text)
+    if(result[0] > 0.5):
+        print(0)
+        return 0
+    else:
+        print(1)
+        return 1
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--text', required=True, help='text for filtering')
+    opt = parser.parse_args()
+    filtering(opt)
